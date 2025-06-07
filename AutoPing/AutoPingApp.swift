9999a68017270afa8ca3828802e7f05ff528e240
Alex Khesin <alex@khesin.com>
2025-06-07 12:18:21 -0400

    Initial Commit

---
+++ AutoPing/AutoPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPingApp.swift
+//  AutoPing
+//
+//  Created by Alex Khesin on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
